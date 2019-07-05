6f90d281b86c8f0c7decc9faca0c250fbc6c4312
Dylan <dyltrz@gmail.com>
2019-07-05 02:55:41 +0000

    Added pagination with will_paginate

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    #@places = Place.all    Replaced with pagination
+    @places = Place.paginate(page: params[:page], per_page: 1)
   end
 
 end
